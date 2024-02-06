1b5be3b6fc6a374d1cb5289544ea21bc52e07c27
Abhi <10572694+gitabhisingh@users.noreply.github.com>
2024-02-05 20:45:03 -0500

    fixed version nos

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
-var express = require('express'),
-app = express(),
-server = require('http').createServer(app),
-io = require('socket.io').listen(server),
-usernames = [];
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const usernames = [];
 
 server.listen(process.env.PORT || 3000);
 
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
+io.on("connection", function (socket) {
+  console.log("Connection Established.");
+  socket.on("new user", function (data, callback) {
+    if (usernames.indexOf(data) != -1) {
+      callback(false);
+    } else {
+      callback(true);
+      socket.username = data;
+      usernames.push(socket.username);
+      updateUsernames();
+    }
+  });
 
-io.sockets.on('connection', function (socket){
-	socket.on('new user', function (data, callback){
-		if(usernames.indexOf(data) != -1){
-			callback(false);
-		} else{
-			callback(true);
-			socket.username = data;
-			usernames.push(socket.username);
-			updateUsernames();
-		}
-	});
+  // Update Usernames
+  function updateUsernames() {
+    io.emit("usernames", usernames);
+  }
 
-	// Update Usernames
-	function updateUsernames(){
-		io.sockets.emit('usernames', usernames);
-	}
+  //Send msg event
+  socket.on("send message", function (data) {
+    io.emit("new message", { msg: data, user: socket.username });
+  });
 
-	//Send msg event
-	socket.on('send message', function (data){
-		io.sockets.emit('new message', {msg: data, user: socket.username});
-	});
-
-	//Disconnect
-	socket.on('disconnect', function (data){
-		if(!socket.username) return;
-		usernames.splice(usernames.indexOf(socket.username), 1);
-		updateUsernames();
-	});
+  //Disconnect
+  socket.on("disconnect", function (data) {
+    if (!socket.username) return;
+    usernames.splice(usernames.indexOf(socket.username), 1);
+    updateUsernames();
+  });
 });
